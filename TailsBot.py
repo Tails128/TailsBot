c28414c1cad725d69befc86bc7cae9df10dccbb2
tails128 <defendifilippo@gmail.com>
2016-11-22 03:48:50 +0100

    added comments and TODO

--- TailsBot.py
+++ TailsBot.py
@@ -6,41 +6,40 @@ import time
 #import random
 #import datetime
 
+
+#TODO: commands should be more complete (including extended syntax)
+commands = ["*motivate*\nSelect a random motivational quote to raise your morale!","*help*\nDisplay all commands","*suggest*\nSuggest a new feature!"]
+helpMessage = "";
+for comm in commands:
+    helpMessage += str(comm) + '\n\n'
+
 def handle(msg):
+#AREA : command line splitting
+    #TODO: IMPROVE SECURITY!!! (parsing, etc...)
     chat_id = msg['chat']['id']
-    command = msg['text'].split(" ",1)[0]
+    command = msg['text'].split(" ",1)[0]   #split command and argument
     sender = msg['from']['username']
     sendHour = msg['date']
     if(len(msg['text'].split(" ",1)) > 1):
         argument = msg['text'].split(" ",1)[1].strip(" ")
-    else:
+    else:           # if there's no argument, argument becomes ""
         argument =""
-
-    commands = ["*motivate*\nSelect a random motivational quote to raise your morale!","*help*\nDisplay all commands","*suggest*\nSuggest a new feature!"]
-    helpMessage = "";
-    for comm in commands:
-        helpMessage += str(comm) + '\n\n'
-
-
-    print 'Got command: %s' % command
-#leaving some comments for basic functions here
-#    if command == '/roll':
-#        bot.sendMessage(chat_id,motivationalThings[random.randint(0,25)])
-#    elif command == '/time':
-#        bot.sendMessage(chat_id, str(datetime.datetime.now()))
-    if command == '/motivate' or command == '/motivate@TailsBot':
+#AREA : command line execution
+    print 'Got command: %s' % command       #may be removed or commented in future
+    if command == '/motivate' or command == '/motivate@TailsBot':           #TODO: search for other rerpos
     	req = requests.post('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
     	req = req.json()
     	bot.sendMessage(chat_id, str(req['quoteText']) + str('\n') + '_' + str(req['quoteAuthor']) + '_', parse_mode = "Markdown")
-    elif command == '/help' or command == '/help@TailsBot':
+    elif command == '/help' or command == '/help@TailsBot':                 #simple help
         bot.sendMessage(chat_id, str(helpMessage), parse_mode = "Markdown")
-    elif command == '/suggest' or command == '/suggest@TailsBot':
-        f = open("suggestions.txt","a")
+    elif command == '/suggest' or command == '/suggest@TailsBot':       #TODO: in case /suggest is empty require a text
+        f = open("suggestions.txt","a")     #TODO it may be nice to do a binary file for analysis, i guess?
         if argument != "":
             f.write("User:\t" + str(sender) + "\nDate:\t" + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(sendHour)) + "\ncontent:\t" + argument + "\n\n");
             bot.sendMessage(chat_id, "Suggestion\n\n__" + argument + "__\n\nSuccessfully added to the list", parse_mode = "Markdown")
         else:
             bot.sendMessage(chat_id, "Please, add your suggestion after the command!\nEs:\n/suggest this is my hint!")
+#AREA : bot execution
 bot = telepot.Bot(os.environ['TAILS_KEY'])
 bot.message_loop(handle)
 print 'I am listening ...'
