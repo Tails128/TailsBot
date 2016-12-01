954b604d4a42672da1eb2462a723bdd32d541b82
tails128 <defendifilippo@gmail.com>
2016-12-01 23:44:10 +0100

    added a minimal exception report on /motivate

--- app.py
+++ app.py
@@ -28,7 +28,11 @@ def handle(msg):
     print 'Got command: %s' % command       #may be removed or commented in future
     if command == '/motivate' or command == '/motivate@TailsBot':           #TODO: search for other rerpos
     	req = requests.post('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
-    	req = req.json()
+        try:
+            req = req.json()
+        except Exception as req:
+            f= open("errors.txt","a")
+            f.write("User:\t" + str(sender) + "\nDate:\t" + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(sendHour)) + "\ncontent:\t" + req + "\nexception\t" + req + "\n\n");
     	bot.sendMessage(chat_id, str(req['quoteText']) + str('\n') + '_' + str(req['quoteAuthor']) + '_', parse_mode = "Markdown")
     elif command == '/help' or command == '/help@TailsBot':                 #simple help
         bot.sendMessage(chat_id, str(helpMessage), parse_mode = "Markdown")
